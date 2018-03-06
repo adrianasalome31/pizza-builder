0fcaab9738fe015fa2a862ec96eed1a10d8d7b53
Manuel Torrez <mastervado98@gmail.com>
2018-03-06 15:51:16 -0600

    Agregar sobreexposición de ingredientes

--- app/assets/javascripts/crear.js
+++ app/assets/javascripts/crear.js
@@ -1,3 +1,10 @@
-function mostrarIngrediente(ingrediente) {
-    $('#'+ingrediente).show("fast");
-}
+//= require jquery3
+
+    function mostrarIngrediente(ingrediente_id, ingrediente){
+        $('#'+ingrediente_id).change(function (){
+            if (this.checked)
+                $('#'+ingrediente).fadeIn('slow');
+            else
+                $('#'+ingrediente).fadeOut('slow');
+        });
+    }
