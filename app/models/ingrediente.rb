8d6725762f6099996a6c4823a3b199572f4d1a14
Manuel Torrez <mastervado98@gmail.com>
2018-03-15 15:54:35 -0600

    Cambiar tamaños de imágenes por defecto en ingredientes

--- app/models/ingrediente.rb
+++ app/models/ingrediente.rb
@@ -1,5 +1,5 @@
 class Ingrediente < ApplicationRecord
-    has_attached_file :imagen, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :imagen, styles: { small: "200x200#", medium: "300x300>", thumb: "512x512#" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :imagen, content_type: /\Aimage\/.*\z/
 
     belongs_to :tipo
